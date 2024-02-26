80779207d1e77c21b069d48ec7fa21d1f587b52d
Guillermina Fara <faramagui94@gmail.com>
2024-02-26 17:36:04 +0100

    agrega ficheros

---
+++ Ficheros/ficheros/rmj.java
@@ -0,0 +1,13 @@
+package ficheros;
+
+/**
+ * rmj
+ */
+public class rmj {
+    public static void main(String[] args) {
+
+        String fichero = args[0];
+        System.out.println(fichero);
+    }
+
+}
