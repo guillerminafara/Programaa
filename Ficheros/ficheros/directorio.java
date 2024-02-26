a4a3875d55d5774463f45bb122c390c16c439bf4
Guillermina Fara <faramagui94@gmail.com>
2024-02-26 18:32:11 +0100

    18:31

---
+++ Ficheros/ficheros/directorio.java
@@ -0,0 +1,20 @@
+/*2. Crear un programa que a partir de un nombre pasado como
+argumento diga si es un fichero o un directorio y muestre la ruta
+absoluta, el tamaño y sus permisos */
+
+import java.io.File;
+
+public class directorio {
+    public static void main(String[] args) {
+        String nombre= args[0];
+        File archivo= new File(nombre);
+        if(archivo.isDirectory()){
+            System.out.printf("el archivo %s es un directorio\n su ruta absoluta es: %s \n Tamaño: %s \n",archivo.getName(), archivo.getAbsolutePath(),archivo.getTotalSpace());
+            System.out.printf(" Ejecución: %b\n Lectura: %b\n Escritura: %b\n",archivo.canExecute(),archivo.canRead(),archivo.canWrite());
+       
+        }else if(archivo.isFile()){
+            System.out.printf("el archivo %s es un fichero\n su ruta absoluta es: %s \n Tamaño: %s \n",archivo.getName(), archivo.getAbsolutePath(),archivo.getTotalSpace());
+            System.out.printf(" Ejecución: %b\n Lectura: %b\n Escritura: %b\n",archivo.canExecute(),archivo.canRead(),archivo.canWrite());
+        }
+    }
+}
--- Ficheros/ficheros/rmj.java
+++ Ficheros/ficheros/rmj.java
@@ -1,13 +1,37 @@
 package ficheros;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
- * rmj
+ * 1. Crear un programa en java (llamado rmj) que permita borrar un
+ * fichero pasándole el nombre del fichero como argumento. El script
+ * tendrá la forma:
+ * java rmj fichero.txt
+ * El programa debe comprobar errores como que el fichero no exista
+ * o que sea un directorio.
  */
 public class rmj {
     public static void main(String[] args) {
+        try {
+            String fichero = args[0];
+            System.out.println(fichero);
 
-        String fichero = args[0];
-        System.out.println(fichero);
+            File file = new File(fichero);
+            if (file.exists()) {
+                if (file.isDirectory()) {
+                    System.out.println("no es posible eliminar carpetas");
+                } else if (file.delete()) {
+                    System.out.println("Fichero eliminado: " + file.getName());
+                } else {
+                    System.out.println("Fichero ya eliminado");
+                }
+            }else{
+                System.out.println("no ha ingresado ningún fichero");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
