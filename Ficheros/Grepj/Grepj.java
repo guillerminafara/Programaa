246c53462bf62b086666c00268f26391030ff82a
Guillermina Fara <faramagui94@gmail.com>
2024-03-06 20:28:58 +0100

    06/03

--- Ficheros/Grepj/Grepj.java
+++ Ficheros/Grepj/Grepj.java
@@ -12,26 +12,52 @@ public class Grepj {
     public static void main(String[] args) {
         try {
             String texto = args[0];
-            String palabra=args[1];
-            int cont=0;
+            String palabra = args[1];
+            int cont = 0;
             File ficherito = new File(texto);
             FileReader fichero = new FileReader(ficherito);
             BufferedReader entrada = new BufferedReader(fichero);
-            String[] pruebita = entrada.readLine().split(" ");
-            System.out.println(Arrays.toString(pruebita));
-            for (int i = 0; i < pruebita.length; i++) {
-               if(pruebita[i].contains(palabra)){
+            String pruebita = entrada.readLine();
+            String[] vector = pruebita.split(" ");
+            int linea = 0;
+            int aux = 0;
+            while (pruebita != null) {
+                // System.out.println(pruebita);
+
+                if (pruebita.contains(palabra)) {
                     cont++;
+                    aux = linea + 1;
+                    // leyendo(aux, pruebita, palabra);
+                    System.out.print(aux + ". ");
+                    for (int i = 0; i < vector.length; i++) {
+
+                        if (vector[i].contains(palabra)) {
+                            System.out.print("\u001B[32m" + palabra + "\u001B[0m"+ " ");
+                        }else{
+                            System.out.print(vector[i]+ " ");
+                        }
+                        
+                    }
+                    System.out.println();
                 }
+
+                linea++;
+                pruebita = entrada.readLine();
             }
-            for (int i = 0; i < cont; i++) {
-                String devolucion= cont+" "+ pruebita[i];
-                System.out.println(devolucion);
-            }
-            
+
+            // for (int i = 0; i < cont; i++) {
+            // String devolucion = " " + pruebita;
+            // System.out.println(devolucion);
+
+            // }
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    public static void leyendo(int aux, String pruebita, String palabra) {
+
     }
 }
