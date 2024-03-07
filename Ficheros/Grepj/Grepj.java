ac3f6eb9b1f2b29f07b003c07a35f8106393cfde
Guillermina Fara <faramagui94@gmail.com>
2024-03-07 18:12:39 +0100

    subir

--- Ficheros/Grepj/Grepj.java
+++ Ficheros/Grepj/Grepj.java
@@ -30,13 +30,44 @@ public class Grepj {
                     for (int i = 0; i < vector.length; i++) {
 
                         if (vector[i].contains(palabra)) {
-                          //.  recorta = Arrays.toString(vector[i].split(recorta));
-                           
-                           //System.out.println( "leyendo recortre"+ recorta);
-                            // recortar(recorta);
-                            System.out.print("\u001B[32m" + palabra + recorta + "\u001B[0m" + " ");
+                            String[] vector2 = vector[i].split(palabra); // le quito a la pabra larga luna
 
-                            //vector[i].indexOf(palabra);
+                            // String lala= vector[i];
+                            // recorta
+                            // System.out.println( "leyendo recortre"+ recorta);
+                            // System.out.println("recortado" + recortar(recorta));
+                            // recorta = recortar(recorta);
+                            // if(vector[i].length()> palabra.length()){
+
+                            // }
+
+                            // vector[i].indexOf(palabra);
+                            if (vector[i].length() == palabra.length()) {
+                                // System.out.print("\u001B[32m" + palabra + "\u001B[0m" );
+                                // System.out.println("señal del destino");
+                                System.out.print("\u001B[32m" + palabra + "\u001B[0m" + " ");
+                            } else {
+                                recorta = Arrays.toString(vector2);
+                                recorta = recortar(recorta);
+                              //  System.out.println(recorta);
+                                System.out.print("\u001B[32m" + palabra + "\u001B[0m" + recorta+" ");
+                                if (vector[i].indexOf(palabra) == 0) { // si el vector comienza con luna
+
+                                }
+                                for (int j = 0; j < vector.length; j++) {
+
+                                    // }else if (vector[i].indexOf(palabra) > 0){
+                                    // // System.out.println("prueba "+ vector[i].indexOf(palabra));
+                                    // System.out.print("\u001B[32m" + vector[i] + "\u001B[0m");
+                                    // System.out.print(vector2[vector[i].indexOf(palabra)]);
+
+                                    // // if (vector[i].indexOf(palabra) == j) {
+                                    // // System.out.print("\u001B[32m" + vector[i] + "\u001B[0m" + " ");
+                                    // //}
+                                    // }
+                                }
+
+                            }
 
                         } else {
 
@@ -53,24 +84,49 @@ public class Grepj {
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 
     public static String recortar(String recorta) {
         for (int j = 0; j < recorta.length(); j++) {
             recorta.trim();
-            if (recorta.charAt(j) == '[') {
+          //  System.out.println("estoy funcionnando");
+            if (recorta.contains("[")) {
                 recorta = recorta.replace("[", "");
             }
-            if (recorta.charAt(j) == ']') {
+            if (recorta.contains("]")) {
                 recorta = recorta.replace("]", "");
             }
-            // if (recorta.charAt(j) == ',') {
+            if (recorta.contains(",")) {
 
-            // recorta = recorta.replace(",", "");
-            // }
+                recorta = recorta.replace(",", "");
+            }
+            if (recorta.contains(" ")) {
+
+                recorta = recorta.replace(" ", "");
+            }
         }
         return recorta;
+    }
+
+    public void orderna(String palabra, String[] vector, int i, String recorta) {
+        vector[i].indexOf(palabra);
+        System.out.println("indice " + vector[i].indexOf(palabra));
+        for (int j = i; j < vector.length; j++) {
+            // if(vector){
+            System.out.print(vector[j]);
+            // }
+
+        }
+        if (vector[i].indexOf(palabra) == 0) {
+            System.out.print("\u001B[32m" + palabra + "\u001B[0m" + recorta + " ");
+        } else {
+            System.out.print(recorta + "\u001B[32m" + palabra + "\u001B[0m" + " ");
+        }
     }
 }
