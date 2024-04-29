1dd57a2d24329a4a0b034d2d11ac5fb6633b76ce
FaraGuillermina <faramagui94@gmail.com>
2024-04-29 12:06:17 +0200

    29/04

--- fx/dinosaurio/dino/src/main/java/dinosaurios/App.java
+++ fx/dinosaurio/dino/src/main/java/dinosaurios/App.java
@@ -2,12 +2,11 @@ package dinosaurios;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,10 +24,17 @@ public class App extends Application {
         this.stage = stage;
         FXMLLoader loadFXML = new FXMLLoader(App.class.getResource("disenio.fxml"));
        // VBox root = new VBox();
+
+       
+   
+
+
+
         scene = new Scene(loadFXML.load());
         stage.setScene(scene);
         stage.show();
 
+      
         // FXMLLoader loadFXML2 = new FXMLLoader(App.class.getResource("scene2.fxml"));
         // scene2 = new Scene(loadFXML2.load());
         // stage.setScene(scene2);
--- fx/dinosaurio/dino/src/main/java/dinosaurios/PrimaryController.java
+++ fx/dinosaurio/dino/src/main/java/dinosaurios/PrimaryController.java
@@ -1,6 +1,6 @@
 package dinosaurios;
 
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 public class PrimaryController implements Initializable{
 
@@ -35,22 +36,28 @@ public class PrimaryController implements Initializable{
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-     try {
+    //  try {
       
-        //Image image = new Image(new FileInputStream("/resources/dinosaurios/dino.jpg"));
-        Image image = new Image(getClass().getResource("/resources/dinosaurios/dino.jpg"));
+    //    // Image image = new Image(new FileInputStream("/resources/dinosaurios/dino.jpg"));
+    //     Image image = new Image(new FileInputStream("C:\\Users\\Fran-PC\\Desktop\\Willy\\DAM\\Programacion\\Unidad9\\Programaa\\fx\\dinosaurio\\dino\\src\\main\\resources\\dinosaurios\\dino.jpg"));
 
-        imageview.setImage(image);
-    } catch (FileNotFoundException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
+    //  //   Image image = new Image(getClass().getResource("/resources/dinosaurios/dino.jpg"));
+
+    //     imageview.setImage(image);
+    // } catch (FileNotFoundException e) {
+    //     // TODO Auto-generated catch block
+    //     e.printStackTrace();
+    // }
         // try {
-        //     JBSalir.setGraphic(new ImageView(new Image(new FileInputStream("salir.png"))));
+        //   //  JBSalir.setGraphic(new ImageView(new Image(new FileInputStream("salir.png"))));
         // } catch (FileNotFoundException e) {
         //     // TODO Auto-generated catch block
         //     e.printStackTrace();
         // }
+
+    
+
+
     }
 
 
